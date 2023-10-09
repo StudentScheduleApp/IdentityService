af5d3b74a65d3ff2e507ed646ec961f8b0f0c348
IlyaKrn <ilyakornoukhov@gmail.com>
2023-10-09 18:35:28 +0300

    refactor

--- src/main/java/com/studentscheduleapp/identityservice/repos/MailRepository.java
+++ src/main/java/com/studentscheduleapp/identityservice/repos/MailRepository.java
@@ -3,6 +3,7 @@ package com.studentscheduleapp.identityservice.repos;
 import com.studentscheduleapp.identityservice.models.api.SendMailRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
@@ -22,6 +23,8 @@ public class MailRepository {
         ResponseEntity<Void> r = restTemplate.postForEntity(mailService + "/api/send", mail, Void.class);
         if(r.getStatusCode().is2xxSuccessful())
             return true;
+        if(r.getStatusCode().equals(HttpStatus.NOT_FOUND))
+            return false;
         if(r.getStatusCode().isError())
             throw new Exception("request to " + mailService + " return code " + r.getStatusCode());
         return false;
