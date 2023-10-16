8b69cc267c3bde65c66b14ff555eb12c6871071c
IlyaKrn <ilyakornoukhov@gmail.com>
2023-10-16 21:48:13 +0300

    authorize

--- src/main/java/com/studentscheduleapp/identityservice/services/AuthorizeUserService.java
+++ src/main/java/com/studentscheduleapp/identityservice/services/AuthorizeUserService.java
@@ -1,12 +1,40 @@
 package com.studentscheduleapp.identityservice.services;
 
+import com.studentscheduleapp.identityservice.models.OutlineMediaComment;
 import com.studentscheduleapp.identityservice.models.api.AuthorizeEntity;
+import com.studentscheduleapp.identityservice.repos.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthorizeUserService {
 
+    @Autowired
+    private CustomLessonRepository customLessonRepository;
+    @Autowired
+    private GroupRepository groupRepository;
+    @Autowired
+    private LessonTemplateRepository lessonTemplateRepository;
+    @Autowired
+    private OutlineMediaCommentRepository outlineMediaCommentRepository;
+    @Autowired
+    private OutlineMediaComment outlineMediaComment;
+    @Autowired
+    private OutlineRepository outlineRepository;
+    @Autowired
+    private ScheduleTemplateRepository scheduleTemplateRepository;
+    @Autowired
+    private SpecificLessonRepository specificLessonRepository;
+    @Autowired
+    private UserRepository userRepository;
+
     public boolean authorize(String token, AuthorizeEntity authorizeEntity){
+
+
+
+
+
+
 
         return true;
     }
