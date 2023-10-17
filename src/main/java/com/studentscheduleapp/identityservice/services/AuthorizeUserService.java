a54dbe5df455f1f15c1cf8971dcc2fab64712005
IlyaKrn <ilyakornoukhov@gmail.com>
2023-10-17 19:48:01 +0300

    authorize

--- src/main/java/com/studentscheduleapp/identityservice/services/AuthorizeUserService.java
+++ src/main/java/com/studentscheduleapp/identityservice/services/AuthorizeUserService.java
@@ -23,7 +23,7 @@ public class AuthorizeUserService {
     @Autowired
     private OutlineMediaCommentRepository outlineMediaCommentRepository;
     @Autowired
-    private OutlineMediaComment outlineMediaComment;
+    private OutlineMediaRepository outlineMediaRepository;
     @Autowired
     private OutlineRepository outlineRepository;
     @Autowired
@@ -48,6 +48,8 @@ public class AuthorizeUserService {
                         return false;
                     break;
                 case "group":
+                    if (u.getRoles().contains(Role.ADMIN))
+                        return true;
                     for (Long l : authorizeEntity.getIds()){
                         boolean fl = false;
                         for (Member m : memberRepository.getByGroupId(l)){
@@ -59,6 +61,8 @@ public class AuthorizeUserService {
                     }
                     break;
                 case "customLesson":
+                    if (u.getRoles().contains(Role.ADMIN))
+                        return true;
                     ArrayList<Long> ids = new ArrayList<>();
                     for (Long l : authorizeEntity.getIds()){
                         CustomLesson cl = customLessonRepository.getById(l);
@@ -66,6 +70,181 @@ public class AuthorizeUserService {
                             ids.add(cl.getGroupId());
                     }
                     for (Long l : ids){
+                        boolean fl = false;
+                        for (Member m : memberRepository.getByGroupId(l)){
+                            if (m.getUserId() == u.getId())
+                                fl = true;
+                        }
+                        if (!fl)
+                            return false;
+                    }
+                    break;
+                case "member":
+                    if (u.getRoles().contains(Role.ADMIN))
+                        return true;
+                    ArrayList<Long> gids = new ArrayList<>();
+                    for (Long l : authorizeEntity.getIds()){
+                        Member cl = memberRepository.getById(l);
+                        if (!gids.contains(cl.getGroupId()))
+                            gids.add(cl.getGroupId());
+                    }
+                    for (Long l : gids){
+                        boolean fl = false;
+                        for (Member m : memberRepository.getByGroupId(l)){
+                            if (m.getUserId() == u.getId())
+                                fl = true;
+                        }
+                        if (!fl)
+                            return false;
+                    }
+                    break;
+                case "specificLesson":
+                    if (u.getRoles().contains(Role.ADMIN))
+                        return true;
+                    ArrayList<Long> ggids = new ArrayList<>();
+                    for (Long l : authorizeEntity.getIds()){
+                        SpecificLesson cl = specificLessonRepository.getById(l);
+                        if (!ggids.contains(cl.getGroupId()))
+                            ggids.add(cl.getGroupId());
+                    }
+                    for (Long l : ggids){
+                        boolean fl = false;
+                        for (Member m : memberRepository.getByGroupId(l)){
+                            if (m.getUserId() == u.getId())
+                                fl = true;
+                        }
+                        if (!fl)
+                            return false;
+                    }
+                    break;
+                case "scheduleTemplate":
+                    if (u.getRoles().contains(Role.ADMIN))
+                        return true;
+                    ArrayList<Long> gggids = new ArrayList<>();
+                    for (Long l : authorizeEntity.getIds()){
+                        ScheduleTemplate cl = scheduleTemplateRepository.getById(l);
+                        if (!gggids.contains(cl.getGroupId()))
+                            gggids.add(cl.getGroupId());
+                    }
+                    for (Long l : gggids){
+                        boolean fl = false;
+                        for (Member m : memberRepository.getByGroupId(l)){
+                            if (m.getUserId() == u.getId())
+                                fl = true;
+                        }
+                        if (!fl)
+                            return false;
+                    }
+                    break;
+                case "lessonTemplate":
+                    if (u.getRoles().contains(Role.ADMIN))
+                        return true;
+                    ArrayList<Long> sids = new ArrayList<>();
+                    for (Long l : authorizeEntity.getIds()){
+                        LessonTemplate cl = lessonTemplateRepository.getById(l);
+                        if (!sids.contains(cl.getScheduleTemplateId()))
+                            sids.add(cl.getScheduleTemplateId());
+                    }
+                    ArrayList<Long> ggggids = new ArrayList<>();
+                    for (Long l : sids){
+                        ScheduleTemplate cl = scheduleTemplateRepository.getById(l);
+                        if (!ggggids.contains(cl.getGroupId()))
+                            ggggids.add(cl.getGroupId());
+                    }
+                    for (Long l : ggggids){
+                        boolean fl = false;
+                        for (Member m : memberRepository.getByGroupId(l)){
+                            if (m.getUserId() == u.getId())
+                                fl = true;
+                        }
+                        if (!fl)
+                            return false;
+                    }
+                    break;
+                case "outline":
+                    if (u.getRoles().contains(Role.ADMIN))
+                        return true;
+                    ArrayList<Long> slids = new ArrayList<>();
+                    for (Long l : authorizeEntity.getIds()){
+                        Outline cl = outlineRepository.getById(l);
+                        if (!slids.contains(cl.getSpecificLessonId()))
+                            slids.add(cl.getSpecificLessonId());
+                    }
+                    ArrayList<Long> gggggids = new ArrayList<>();
+                    for (Long l : slids){
+                        SpecificLesson cl = specificLessonRepository.getById(l);
+                        if (!gggggids.contains(cl.getGroupId()))
+                            gggggids.add(cl.getGroupId());
+                    }
+                    for (Long l : gggggids){
+                        boolean fl = false;
+                        for (Member m : memberRepository.getByGroupId(l)){
+                            if (m.getUserId() == u.getId())
+                                fl = true;
+                        }
+                        if (!fl)
+                            return false;
+                    }
+                    break;
+                case "outlineMedia":
+                    if (u.getRoles().contains(Role.ADMIN))
+                        return true;
+                    ArrayList<Long> oids = new ArrayList<>();
+                    for (Long l : authorizeEntity.getIds()){
+                        OutlineMedia cl = outlineMediaRepository.getById(l);
+                        if (!oids.contains(cl.getOutlineId()))
+                            oids.add(cl.getOutlineId());
+                    }
+                    ArrayList<Long> sllids = new ArrayList<>();
+                    for (Long l : oids){
+                        Outline cl = outlineRepository.getById(l);
+                        if (!sllids.contains(cl.getSpecificLessonId()))
+                            sllids.add(cl.getSpecificLessonId());
+                    }
+                    ArrayList<Long> ggggggids = new ArrayList<>();
+                    for (Long l : sllids){
+                        SpecificLesson cl = specificLessonRepository.getById(l);
+                        if (!ggggggids.contains(cl.getGroupId()))
+                            ggggggids.add(cl.getGroupId());
+                    }
+                    for (Long l : ggggggids){
+                        boolean fl = false;
+                        for (Member m : memberRepository.getByGroupId(l)){
+                            if (m.getUserId() == u.getId())
+                                fl = true;
+                        }
+                        if (!fl)
+                            return false;
+                    }
+                    break;
+                case "outlineMediaComment":
+                    if (u.getRoles().contains(Role.ADMIN))
+                        return true;
+                    ArrayList<Long> ocids = new ArrayList<>();
+                    for (Long l : authorizeEntity.getIds()){
+                        OutlineMediaComment cl = outlineMediaCommentRepository.getById(l);
+                        if (!ocids.contains(cl.getMediaId()))
+                            ocids.add(cl.getMediaId());
+                    }
+                    ArrayList<Long> ooids = new ArrayList<>();
+                    for (Long l : ocids){
+                        OutlineMedia cl = outlineMediaRepository.getById(l);
+                        if (!ooids.contains(cl.getOutlineId()))
+                            ooids.add(cl.getOutlineId());
+                    }
+                    ArrayList<Long> ssllids = new ArrayList<>();
+                    for (Long l : ooids){
+                        Outline cl = outlineRepository.getById(l);
+                        if (!ssllids.contains(cl.getSpecificLessonId()))
+                            ssllids.add(cl.getSpecificLessonId());
+                    }
+                    ArrayList<Long> gggggggids = new ArrayList<>();
+                    for (Long l : ssllids){
+                        SpecificLesson cl = specificLessonRepository.getById(l);
+                        if (!gggggggids.contains(cl.getGroupId()))
+                            gggggggids.add(cl.getGroupId());
+                    }
+                    for (Long l : gggggggids){
                         boolean fl = false;
                         for (Member m : memberRepository.getByGroupId(l)){
                             if (m.getUserId() == u.getId())
